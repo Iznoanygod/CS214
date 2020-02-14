a77bf5c4ad2e97318dba1517961c7113c8f2939d
andrew <mkbandrewp@gmail.com>
2020-02-13 23:08:35 -0500

    initial commit

---
+++ asst0/fileSort.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int insertionSort(void* toSort, int (*comparator)(void*, void*));
+int quicksort(void* toSort, int (*comparator)(void*, void*));
+int stringCompare();
+
+
+int main(int argc, char** argv){
+
+    return 0;
+}
+
+
